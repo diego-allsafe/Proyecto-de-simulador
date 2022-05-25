1b36ef444faf35504bdd815f92e42d5021f52795
diego.data.icloud@gmail.com <diego.data.icloud@gmail.com>
2022-05-25 11:37:57 -0300

    agregue pagina de loguin

---
+++ js/ingreso.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("guardar"),
+  checkbox = document.getElementById("check");
+const email = document.getElementById("email");
+const password = document.getElementById("password"),
+  p = document.querySelector("p");
+btn.innerText = "Registrar";
